08e01d13195ba46ef067c8298de4491987faa9c6
chloe2781 <chloe.nguyen.2708@gmail.com>
2023-10-25 02:06:19 -0400

    blog Written, robot made

--- main.js
+++ main.js
@@ -6,68 +6,62 @@ const playNoise = document.getElementById('more-noise');
 
 playBab.addEventListener('click', function () {
     if (!audioCtx1) {
-        if(audioCtx2){ audioCtx2.suspend();}
+        if(audioCtx2){ 
+            audioCtx2.suspend();
+            playNoise.style.backgroundColor = '#4CAF50';
+        }
+        playBab.style.backgroundColor = 'red';
         babbling_brook();
         return;
     }
     else if (audioCtx1.state === 'suspended') {
-        if(audioCtx2){ audioCtx2.suspend();}
+        if(audioCtx2){ 
+            audioCtx2.suspend();
+            playNoise.style.backgroundColor = '#4CAF50';
+        }
+        playBab.style.backgroundColor = 'red';
         audioCtx1.resume();
     }
     else if (audioCtx1.state === 'running') {
         audioCtx1.suspend();
+        playBab.style.backgroundColor = '#4CAF50';
     }
 }, false);
 
-
 playNoise.addEventListener('click', function () {
     if (!audioCtx2) {
-        if(audioCtx1){ audioCtx1.suspend();}
+        if(audioCtx1){ 
+            audioCtx1.suspend();
+            playBab.style.backgroundColor = '#4CAF50';
+        }
+        playNoise.style.backgroundColor = 'red';
         moreNoise();
         return;
     }
     else if (audioCtx2.state === 'suspended') {
-        if(audioCtx1){ audioCtx1.suspend();}
+        if(audioCtx1){ 
+            audioCtx1.suspend();
+            playBab.style.backgroundColor = '#4CAF50';
+        }
+        playNoise.style.backgroundColor = 'red';
         audioCtx2.resume();
     }
     else if (audioCtx2.state === 'running') {
         audioCtx2.suspend();
+        playNoise.style.backgroundColor = '#4CAF50';
     }
 }, false);
-
-
-function moreNoise(){
-    audioCtx2 = new AudioContext()
-    const oscillator = audioCtx2.createOscillator();
-    oscillator.type = 'sine';
-    oscillator.frequency.value = 440; // Set the frequency to 440Hz (A4)
-
-    // Create a resonant bandpass filter
-    const filter = audioCtx2.createBiquadFilter();
-    filter.type = 'bandpass';
-    filter.frequency.value = 1000; // Set the center frequency to 1000Hz
-    filter.Q.value = 10; // Set the quality factor to 10
-
-    // Connect the oscillator to the filter and the filter to the audio context's destination (e.g., your speakers)
-    oscillator.connect(filter);
-    filter.connect(audioCtx2.destination);
-
-    // Start the oscillator
-    oscillator.start();
-
-}
-
 
 function babbling_brook(){
     audioCtx1 = new AudioContext()
 
     const lowpass1 = audioCtx1.createBiquadFilter()
     lowpass1.type = 'lowpass'
-    lowpass1.frequency.value = 400; //400
+    lowpass1.frequency.value = 400; 
 
     const lowpass2 = audioCtx1.createBiquadFilter()
     lowpass2.type = 'lowpass'
-    lowpass2.frequency.value = 14; //14
+    lowpass2.frequency.value = 14; 
 
     const lowpass3 = audioCtx1.createBiquadFilter()
     lowpass3.type = 'lowpass'
@@ -86,8 +80,6 @@ function babbling_brook(){
 
     const gain2 = audioCtx1.createGain();
     gain2.gain.value = 400;
-
-
 
     const add = new ConstantSourceNode(audioCtx1, {offset: 500})
 
@@ -112,17 +104,44 @@ function babbling_brook(){
     highpass.connect(gain1).connect(audioCtx1.destination);
 
     brownNoise.connect(lowpass2).connect(gain2).connect(highpass.frequency)
-
-
-    // new ////////
     brownNoise.connect(lowpass3).connect(gain3).connect(highpass.frequency)
-    //////////////////////
-
-
-    // gain2.connect(add.offset)
-    // add.connect(highpass.frequency)
 
     // {RHPF.ar(LPF.ar(BrownNoise.ar(), 400), LPF.ar(BrownNoise.ar(), 14) * 400 + 500, 0.03, 0.1)}.play
 
     brownNoise.start(0);
+}
+
+function moreNoise(){
+    audioCtx2 = new AudioContext()
+    const oscillator = audioCtx2.createOscillator();
+    oscillator.type = 'sine';
+    oscillator.frequency.value = 1000 + Math.random() * 400;
+
+    // Create a resonant bandpass filter
+    const filter = audioCtx2.createBiquadFilter();
+    filter.type = 'bandpass';
+    filter.frequency.value = 500 + Math.random() * 1000; // Set the center frequency to 1000Hz
+    filter.Q.value = 2; // Set the quality factor to 10
+
+    const gain = audioCtx2.createGain();
+    gain.gain.value = 0.5 + Math.random() * 0.8;
+
+    const delay = audioCtx2.createDelay();
+    delay.delayTime.value = 0.1 + Math.random() * 0.4;
+
+    // Connect the oscillator to the filter and the filter to the audio context's destination (e.g., your speakers)
+    oscillator.connect(filter);
+    filter.connect(gain);
+    gain.connect(delay);
+    delay.connect(audioCtx2.destination);
+
+    // Start the oscillator
+    oscillator.start();
+
+    const now = audioCtx2.currentTime;
+    for (let i = 0; i < 200; i++) {
+        const time = now + i*0.11;
+        const frequency = 60 + Math.random() * 1000;
+        oscillator.frequency.setValueAtTime(frequency, time);
+    }
 }
